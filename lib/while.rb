c9cd77ad8ae0ba581c4344f250e8fe3d92ad9c6d
Emily Willow <emily.shapiera@gmail.com>
2020-11-23 16:29:24 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,8 +1,8 @@
 def using_while
   levitation_force = 6
-while counter < 10
+while levitation_force < 10
   puts "Wingardium Leviosa"
-  counter += 1
+  levitation_force += 1
   #your code here
 end
 end
